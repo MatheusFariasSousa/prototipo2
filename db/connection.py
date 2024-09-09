4c1a400b9e0c340d5b96768a451068f10d98f63d
MatheusFariasSousa <143126173+MatheusFariasSousa@users.noreply.github.com>
2024-09-09 12:10:13 -0300

    postgres

--- db/connection.py
+++ db/connection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DB_URL = "sqlite:///teste.db"
+DB_URL = "postgresql+psycopg://postgres:adminpass@127.0.0.1:5432/testando_db"
 
 engine = create_engine(DB_URL)
 
