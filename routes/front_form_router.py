8c05ebe89e133c08492e211e4cfde1dc804f25c4
MatheusFariasSousa <143126173+MatheusFariasSousa@users.noreply.github.com>
2024-09-21 13:50:42 -0300

    teste

--- routes/front_form_router.py
+++ routes/front_form_router.py
@@ -1,16 +1,23 @@
-from fastapi import APIRouter,Form,Depends,status
+from fastapi import APIRouter,Form,Depends,status,HTTPException
 from fastapi.responses import RedirectResponse
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from db.deps import get_conection
 from db.model import Notes
+from db.model import User   
 from schema.user_schema import User_schema,User_Schema_Output
 from schema.notes_schema import Note_Schema
 from use_case.user_use_cases import User_use_cases
 from use_case.notes_use_cases import Notes_Use_Case
+from security.user import create_access_token,get_current_user
 
-front_router = APIRouter(prefix="/front")
+from passlib.context import CryptContext
+
+front_router = APIRouter(prefix="/front",tags=["Front"])
+
+crypt =CryptContext(schemes=["sha256_crypt"])
 
 
 
@@ -28,16 +35,38 @@ def post_front(fname:str=Form(...),cpf:str= Form(...),password:str= Form(...),db
     uc.post_user(person)
     return person
 
+@front_router.post("/get-token")
+def get_token(lname:str=Form(...),lpassword:str=Form(...),db_session:Session = Depends(get_conection)):
+   
+    user = db_session.query(User).where(User.name==lname).first()
+    if not user or not crypt.verify(lpassword,user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Usuario ou senha incorreto")
+    access_token = create_access_token(data={"sub":lname})
+    return {"access_token": access_token,"token_type":"bearer"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @front_router.post("/notes")
-def post_note(title:str=Form(...),note:str=Form(...),db_session:Session = Depends(get_conection)):
+def post_note(title:str=Form(...),note:str=Form(...),db_session:Session = Depends(get_conection),current_user:User = Depends(get_current_user)):
     notation = Note_Schema(title=title,text=note)
     uc= Notes_Use_Case(db_session=db_session)
-    uc.post_not(notation)
+    uc.post_not(notation,current_user)
     return RedirectResponse(url="/front/notes", status_code=status.HTTP_303_SEE_OTHER)
 
 @front_router.get("/notes")
-def read_notes(request:Request,db_session:Session = Depends(get_conection)):
-    notes = db_session.query(Notes).all()
+def read_notes(request:Request,db_session:Session = Depends(get_conection),current_user:User = Depends(get_current_user)):
+    notes = db_session.query(Notes).where(Notes.user_id==current_user.id)
     return templates.TemplateResponse("oi.html",{"request":request,"notes":notes})
 
     
